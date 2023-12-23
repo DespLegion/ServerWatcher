a9546c99cb242b990fb8c6f6d61eb4d3dcac3162
DespLegion <tools55000@gmail.com>
2023-12-23 11:54:49 +0300

    2.0.0a 23.12.23/2

--- src/core/get_os.py
+++ src/core/get_os.py
@@ -14,15 +14,16 @@ def get_os_name():
             return f"{platform_name} {platform.release()}"
         elif platform_name == "Linux":
             import distro
-            print(f"Distro id - {distro.id()}")
+            # print(f"Distro id - {distro.id()}")
             print(f"Distro version - {distro.version()}")
             print(f"Distro name standart - {distro.name()}")
-            print(f"Distro name pretty - {distro.name(pretty=True)}")
+            # print(f"Distro name pretty - {distro.name(pretty=True)}")
             # os_name = os_human_name(distro.id())
-            return os_human_name(distro.id()) if os_human_name(distro.id()) else distro.id().capitalize()
+            # return os_human_name(distro.id()) if os_human_name(distro.id()) else distro.id().capitalize()
             # if os_human_name(distro.id()):
             #     return os_human_name(distro.id())
             # return distro.id().capitalize()
+            return f"{distro.name()} - {distro.version()}"
         else:
             return "Unsupported OS!"
     except Exception as err:
